0557cdc29783980431465c13ce33e3a091938395
Luciano Bueno <glamero@gmail.com>
2018-08-13 20:40:18 -0300

    bump

--- index.js
+++ index.js
@@ -3,42 +3,42 @@
 var Moo = require("mootools"),
 	path = require('path'),
 	fs = require('fs');
-
+	
 var Logger = require('node-express-logger');
 		//Authorization = require('node-express-authorization');
 
 
 module.exports = new Class({
   Implements: [Options, Events],
-
+  
   ON_LOAD_APP: 'onLoadApp',
   ON_USE: 'onUse',
   ON_USE_APP: 'onUseApp',
-
-
+  
+  
   logger: null,
   //authorization:null,
   //authentication: null,
   _merged_apps: {},
-
+  
   options: {
-
+			
 		id: '',
 		path: '',
-
-
+		
+		
   },
   initialize: function(options){
-
+		
 		if(
 			this.options
 			&& this.options.path
 			&& this.options.path.indexOf('/') == 0
 		)
 			delete options.path
-
+			
 		this.setOptions(options);//override default options
-
+		
 		/**
 		 * logger
 		 *  - start
@@ -46,7 +46,7 @@ module.exports = new Class({
 		if(this.options.logs){
 			//console.log('----instance----');
 			//console.log(this.options.logs);
-
+			
 			if(typeof(this.options.logs) == 'class'){
 				var tmp_class = this.options.logs;
 				this.logger = new tmp_class(this, {});
@@ -64,36 +64,36 @@ module.exports = new Class({
 				this.logger = new Logger(this, this.options.logs);
 				//app.use(this.logger.access());
 			}
-
+			
 			//app.use(this.logger.access());
-
+			
 			//console.log(this.logger.instance);
 		}
-
-
+		
+		
 		/**
 		 * logger
 		 *  - end
 		 * **/
-
-
-
-
+		
+		
+		
+		
   },
   log: function(name, type, string){},
   profile: function(profile){},
 	use: function(mount, app){
-
+		
 		this.fireEvent(this.ON_USE, [mount, app, this]);
-
+		
 		if(typeOf(app) == 'class' || typeOf(app) == 'object')
 			this.fireEvent(this.ON_USE_APP, [mount, app, this]);
-
-
+		
+		
 		//if(typeOf(app) == 'object'){
-
+	
 			//if(this.authorization && app.options.authorization && app.options.authorization.config){
-
+				
 				//var rbac = fs.readFileSync(app.options.authorization.config , 'ascii');
 				//app.options.authorization.config = rbac;
 				//this.authorization.processRules(
@@ -102,40 +102,40 @@ module.exports = new Class({
 					//)
 				//);
 			//}
-
+			
 
 		//}
 		//else{
-
+			
 		//}
-
+		
 		var to_append = this._merge(mount, app);
-
+		
 		var key = Object.keys(to_append)[0];
 		if(this[key]){//an app has beend loaded on that key
 			var tmp = this[key];
 			if(typeOf(tmp) == 'object'){
 				delete this[key];
-
+				
 				Object.append(to_append[key], tmp);
 			}
-
+			
 			Object.append(this, to_append);
 		}
 		else{
 			Object.append(this, to_append);
 		}
-
+		
   },
   _merge: function(mount, app){
-
+		
 		if(Object.getLength(mount) > 0){
 			Object.each(mount, function(value, key){
-
+				
 				mount[key] = this._merge(value, app);
-
+				
 			}.bind(this));
-
+			
 			return mount;
 		}
 		else{
@@ -144,53 +144,50 @@ module.exports = new Class({
 	},
   load: function(wrk_dir, options){
 		options = options || {};
-
+		
 		var get_options = function(options){
-
+			
 			/**
 			 * subapps will re-use main app logger
 			 * */
-			if(this.logger)
+			if(this.logger)	
 				options.logs = this.logger;
-
+			
 			return options;
-
+		
 		}.bind(this);
-
-
+		
+		
 		fs.readdirSync(wrk_dir).forEach(function(file) {
 
 			var full_path = path.join(wrk_dir, file);
-
-
+			
+			
 			if(! (file.charAt(0) == '.')){//ommit 'hiden' files
 				//console.log('-------');
-
+				
 				//console.log('app load: '+ file);
 				var app = null;
 				var id = '';//app id
 				var mount = {};
 				let app_path = '/';
-
+				
 				if(fs.statSync(full_path).isDirectory() == true){//apps inside dir
-
+					
 					////console.log('dir app: '+full_path);
-
+					
 					var dir = file;//is dir
-
-
+					
+					
 					fs.readdirSync(full_path).forEach(function(file) {//read each file in directory
-
+						
 						app_path = this.options.path;
-
+						
 						if(path.extname(file) == '.js' && ! (file.charAt(0) == '.')){
-
+							
 							////console.log('app load js: '+ file);
-							// app = require(path.join(full_path, file));
-							// Can't use `require` because of Webpack being annoying:
-							// https://github.com/webpack/webpack/issues/196
-							app = JSON.parse(fs.readFileSync(path.join(full_path, file), 'utf8'))
-
+							app = require(path.join(full_path, file));
+							
 							mount[dir] = {};
 							if(file == 'index.js'){
 								app_path += dir;
@@ -199,9 +196,9 @@ module.exports = new Class({
 								mount[dir][path.basename(file, '.js')] = {};
 								app_path += dir+'/'+path.basename(file, '.js');
 							}
-
+							
 							if(typeOf(app) == 'class'){//mootools class
-
+								
 								this.fireEvent(this.ON_LOAD_APP, [app, this]);
 								options.path = app_path;
 								try{
@@ -210,12 +207,12 @@ module.exports = new Class({
 								catch(e){
 									//console.log(e);
 								}
-
+								
 							}
 							else{//nodejs module
 								////console.log('express app...nothing to do');
 							}
-
+							
 							this.use(mount, app);
 						}
 
@@ -223,20 +220,16 @@ module.exports = new Class({
 
 				}
 				else if(path.extname( file ) == '.js'){// single js apps
-
-					// app = require(full_path);
-					// Can't use `require` because of Webpack being annoying:
-					// https://github.com/webpack/webpack/issues/196
-					app = JSON.parse(fs.readFileSync(full_path, 'utf8'))
-
+					
+					app = require(full_path);
 					//id = path.basename(file, '.js');
 					id = path.basename(wrk_dir);
-
+					
 					if(file == 'index.js'){
-
+						
 						mount[id] = {};
 						app_path = id;
-
+					
 						//console.log(mount);
 						//throw new Error();
 					}
@@ -246,18 +239,18 @@ module.exports = new Class({
 						mount[id][name] = {};
 						//app_path = id;
 						app_path = id+'/'+name;
-
+						
 						//console.log(mount);
-
+						
 					}
-
+					
 					if(typeOf(app) == 'class'){//mootools class
-
+						
 						this.fireEvent(this.ON_LOAD_APP, [app, this]);
-
+						
 						options.path = app_path;
-
-						try{
+						
+						try{		
 							app = new app(get_options(options));
 						}
 						catch(e){
@@ -267,19 +260,21 @@ module.exports = new Class({
 					else{//nodejs module
 						////console.log('express app...nothing to do');
 					}
-
+					
 					//console.log(mount);
 					this.use(mount, app);
-
+					
 				}
-
-
+				
+				
 			}
 		}.bind(this))
-
+		
 		Object.append(this, this._merged_apps);
-
+		
 	},
-
-
+  
+	
 });
+
+
