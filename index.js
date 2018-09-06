e5909779d6d04ea377b28030140c17a889c69773
Luciano Bueno <glamero@gmail.com>
2018-09-06 14:47:33 -0300

    bump

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var Logger = require('node-express-logger');
 		//Authorization = require('node-express-authorization');
 
 
-module.exports = new Class({
+var App = new Class({
   Implements: [Options, Events],
   
   ON_LOAD_APP: 'onLoadApp',
@@ -141,140 +141,151 @@ module.exports = new Class({
 		else{
 			return app;
 		}
-	},
-  load: function(wrk_dir, options){
-		options = options || {};
-		
-		var get_options = function(options){
-			
-			/**
-			 * subapps will re-use main app logger
-			 * */
-			if(this.logger)	
-				options.logs = this.logger;
-			
-			return options;
-		
-		}.bind(this);
-		
-		
-		fs.readdirSync(wrk_dir).forEach(function(file) {
+	}
+  
+  
+	
+});
 
-			var full_path = path.join(wrk_dir, file);
-			
-			
-			if(! (file.charAt(0) == '.')){//ommit 'hiden' files
-				//console.log('-------');
+/**
+ * https://stackoverflow.com/questions/17575790/environment-detection-node-js-or-browser
+ **/
+var isNode=new Function("try {return this===global;}catch(e){return false;}");
+module.exports = function(){
+	if(isNode()){
+		App.implement({
+			load: function(wrk_dir, options){
+				options = options || {};
 				
-				//console.log('app load: '+ file);
-				var app = null;
-				var id = '';//app id
-				var mount = {};
-				let app_path = '/';
+				var get_options = function(options){
+					
+					/**
+					 * subapps will re-use main app logger
+					 * */
+					if(this.logger)	
+						options.logs = this.logger;
+					
+					return options;
 				
-				if(fs.statSync(full_path).isDirectory() == true){//apps inside dir
+				}.bind(this);
+				
+				
+				fs.readdirSync(wrk_dir).forEach(function(file) {
+
+					var full_path = path.join(wrk_dir, file);
 					
-					////console.log('dir app: '+full_path);
 					
-					var dir = file;//is dir
-					
-					
-					fs.readdirSync(full_path).forEach(function(file) {//read each file in directory
+					if(! (file.charAt(0) == '.')){//ommit 'hiden' files
+						//console.log('-------');
 						
-						app_path = this.options.path;
+						//console.log('app load: '+ file);
+						var app = null;
+						var id = '';//app id
+						var mount = {};
+						let app_path = '/';
 						
-						if(path.extname(file) == '.js' && ! (file.charAt(0) == '.')){
+						if(fs.statSync(full_path).isDirectory() == true){//apps inside dir
 							
-							////console.log('app load js: '+ file);
-							app = require(path.join(full_path, file));
+							////console.log('dir app: '+full_path);
 							
-							mount[dir] = {};
+							var dir = file;//is dir
+							
+							
+							fs.readdirSync(full_path).forEach(function(file) {//read each file in directory
+								
+								app_path = this.options.path;
+								
+								if(path.extname(file) == '.js' && ! (file.charAt(0) == '.')){
+									
+									////console.log('app load js: '+ file);
+									app = require(path.join(full_path, file));
+									
+									mount[dir] = {};
+									if(file == 'index.js'){
+										app_path += dir;
+									}
+									else{
+										mount[dir][path.basename(file, '.js')] = {};
+										app_path += dir+'/'+path.basename(file, '.js');
+									}
+									
+									if(typeOf(app) == 'class'){//mootools class
+										
+										this.fireEvent(this.ON_LOAD_APP, [app, this]);
+										options.path = app_path;
+										try{
+											app = new app(get_options(options));
+										}
+										catch(e){
+											//console.log(e);
+										}
+										
+									}
+									else{//nodejs module
+										////console.log('express app...nothing to do');
+									}
+									
+									this.use(mount, app);
+								}
+
+							}.bind(this));//end load single JS files
+
+						}
+						else if(path.extname( file ) == '.js'){// single js apps
+							
+							app = require(full_path);
+							//id = path.basename(file, '.js');
+							id = path.basename(wrk_dir);
+							
 							if(file == 'index.js'){
-								app_path += dir;
+								
+								mount[id] = {};
+								app_path = id;
+							
+								//console.log(mount);
+								//throw new Error();
 							}
 							else{
-								mount[dir][path.basename(file, '.js')] = {};
-								app_path += dir+'/'+path.basename(file, '.js');
+								let name = path.basename(file, '.js');
+								mount[id] = {};
+								mount[id][name] = {};
+								//app_path = id;
+								app_path = id+'/'+name;
+								
+								//console.log(mount);
+								
 							}
 							
 							if(typeOf(app) == 'class'){//mootools class
 								
 								this.fireEvent(this.ON_LOAD_APP, [app, this]);
+								
 								options.path = app_path;
-								try{
+								
+								try{		
 									app = new app(get_options(options));
 								}
 								catch(e){
 									//console.log(e);
 								}
-								
 							}
 							else{//nodejs module
 								////console.log('express app...nothing to do');
 							}
 							
+							//console.log(mount);
 							this.use(mount, app);
+							
 						}
-
-					}.bind(this));//end load single JS files
-
-				}
-				else if(path.extname( file ) == '.js'){// single js apps
-					
-					app = require(full_path);
-					//id = path.basename(file, '.js');
-					id = path.basename(wrk_dir);
-					
-					if(file == 'index.js'){
 						
-						mount[id] = {};
-						app_path = id;
-					
-						//console.log(mount);
-						//throw new Error();
-					}
-					else{
-						let name = path.basename(file, '.js');
-						mount[id] = {};
-						mount[id][name] = {};
-						//app_path = id;
-						app_path = id+'/'+name;
-						
-						//console.log(mount);
 						
 					}
-					
-					if(typeOf(app) == 'class'){//mootools class
-						
-						this.fireEvent(this.ON_LOAD_APP, [app, this]);
-						
-						options.path = app_path;
-						
-						try{		
-							app = new app(get_options(options));
-						}
-						catch(e){
-							//console.log(e);
-						}
-					}
-					else{//nodejs module
-						////console.log('express app...nothing to do');
-					}
-					
-					//console.log(mount);
-					this.use(mount, app);
-					
-				}
+				}.bind(this))
 				
+				Object.append(this, this._merged_apps);
 				
 			}
-		}.bind(this))
-		
-		Object.append(this, this._merged_apps);
-		
-	},
-  
-	
-});
-
-
+		});
+	}
+	return App
+}
